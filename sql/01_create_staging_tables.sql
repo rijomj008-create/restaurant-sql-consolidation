c145f52380f560c17e898e3bf33ffd478fafba75
rijomj008-create <rijomj008@gmail.com>
2025-10-01 12:57:42 +0100

    Update 01_create_staging_tables.sql

--- sql/01_create_staging_tables.sql
+++ sql/01_create_staging_tables.sql
@@ -1,1 +1,10 @@
+-- Putting tables into the new schema
+
+Drop Table If Exists staging.master_daily_raw;
+Create Table staging.master_daily_raw As 
+Select * From public.master_daily;
+
+Drop Table If Exists staging.staffing_raw;
+Create Table staging.staffing_raw As 
+Select * From public.staffing_by_shift;
 
